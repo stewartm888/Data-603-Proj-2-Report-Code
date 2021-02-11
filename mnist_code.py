06b88dd3d7c216746b2b188d3c0e95607d146ef1
Fred Watts <44913109+stewartm888@users.noreply.github.com>
2021-02-11 18:01:43 -0500

    Update mnist_code.py

--- mnist_code.py
+++ mnist_code.py
@@ -122,18 +122,26 @@ for x in typelist:
         print("OPTIMIZER {} ACC%  -- {}".format(digotmilist[digotmi_acc[typelist.index(x)].index(y)],y))
     print("\n------------\n")
 
-
-
-### MNIST
+########################################################
 ########################################################
 
-############################
-### Architecture #1 CNN ####
-############################
+# ----------------- MNIST  ----------------- 
+########################################################
+########################################################
 
-# example of loading the mnist dataset
+
 from keras.datasets import mnist
-from matplotlib import pyplot
+epochlist = [2,5,10,15,20]
+otmilist = ["ADAM","STOC_GRAD","ADAGRAD","RMSPROP"]
+typelist = ["CNN","FF","RRN"]
+
+
+
+
+#Mode 1: CNN 
+############
+
+
 # load dataset
 (trainX, trainy), (testX, testy) = mnist.load_data()
 # summarize loaded dataset
@@ -142,24 +150,17 @@ print('Test: X=%s, y=%s' % (testX.shape, testy.shape))
 # plot first few images
 for i in range(9):
     # define subplot
-    pyplot.subplot(330 + 1 + i)
+    plt.subplot(330 + 1 + i)
     # plot raw pixel data
-    pyplot.imshow(trainX[i], cmap=pyplot.get_cmap('gray'))
+    plt.imshow(trainX[i], cmap=plt.get_cmap('gray'))
 # show the figure
-pyplot.show()
+plt.show()
 
 # record model performance on a validation dataset during training
 history = model.fit(..., validation_data=(valX, valY))
 
 # example of k-fold cv for a neural net
 data = ...
-# prepare cross validation
-kfold = KFold(5, shuffle=True, random_state=1)
-# enumerate splits
-for train_ix, test_ix in kfold.split(data):
-        model = ...
-    ...
-    
 
 # load dataset
 (trainX, trainY), (testX, testY) = mnist.load_data()
@@ -194,26 +195,6 @@ def load_dataset():
     testY = to_categorical(testY)
     return trainX, trainY, testX, testY
 
-
-# convert from integers to floats
-train_norm = train.astype('float32')
-test_norm = test.astype('float32')
-# normalize to range 0-1
-train_norm = train_norm / 255.0
-test_norm = test_norm / 255.0
-
-# scale pixels
-def prep_pixels(train, test):
-    # convert from integers to floats
-    train_norm = train.astype('float32')
-    test_norm = test.astype('float32')
-    # normalize to range 0-1
-    train_norm = train_norm / 255.0
-    test_norm = test_norm / 255.0
-    # return normalized images
-    return train_norm, test_norm
-
-
 # define cnn model
 def define_model():
     model = Sequential()
@@ -241,99 +222,6 @@ def define_model():
     return model
 
 
-# evaluate a model using k-fold cross-validation
-def evaluate_model(dataX, dataY, n_folds=5):
-    scores, histories = list(), list()
-    # prepare cross validation
-    kfold = KFold(n_folds, shuffle=True, random_state=1)
-    # enumerate splits
-    for train_ix, test_ix in kfold.split(dataX):
-        # define model
-        model = define_model()
-        # select rows for train and test
-        trainX, trainY, testX, testY = dataX[train_ix], dataY[train_ix], dataX[test_ix], dataY[test_ix]
-        # fit model
-        history = model.fit(trainX, trainY, epochs=10, batch_size=32, validation_data=(testX, testY), verbose=0)
-        # evaluate model
-        _, acc = model.evaluate(testX, testY, verbose=0)
-        print('> %.3f' % (acc * 100.0))
-        # stores scores
-        scores.append(acc)
-        histories.append(history)
-    return scores, histories
-
-
-# evaluate a model using k-fold cross-validation
-def evaluate_model(dataX, dataY, n_folds=5):
-    scores, histories = list(), list()
-    # prepare cross validation
-    kfold = KFold(n_folds, shuffle=True, random_state=1)
-    # enumerate splits
-    for train_ix, test_ix in kfold.split(dataX):
-        # define model
-        model = define_model()
-        # select rows for train and test
-        trainX, trainY, testX, testY = dataX[train_ix], dataY[train_ix], dataX[test_ix], dataY[test_ix]
-        # fit model
-        history = model.fit(trainX, trainY, epochs=10, batch_size=32, validation_data=(testX, testY), verbose=0)
-        # evaluate model
-        _, acc = model.evaluate(testX, testY, verbose=0)
-        print('> %.3f' % (acc * 100.0))
-        # stores scores
-        scores.append(acc)
-        histories.append(history)
-    return scores, histories
-
-
-# plot diagnostic learning curves
-def summarize_diagnostics(histories):
-    for i in range(len(histories)):
-        # plot loss
-        pyplot.subplot(2, 1, 1)
-        pyplot.title('Cross Entropy Loss')
-        pyplot.plot(histories[i].history['loss'], color='blue', label='train')
-        pyplot.plot(histories[i].history['val_loss'], color='orange', label='test')
-        # plot accuracy
-        pyplot.subplot(2, 1, 2)
-        pyplot.title('Classification Accuracy')
-        pyplot.plot(histories[i].history['accuracy'], color='blue', label='train')
-        pyplot.plot(histories[i].history['val_accuracy'], color='orange', label='test')
-    pyplot.show()
-
-# plot diagnostic learning curves
-def summarize_diagnostics(histories):
-    for i in range(len(histories)):
-        # plot loss
-        pyplot.subplot(2, 1, 1)
-        pyplot.title('Cross Entropy Loss')
-        pyplot.plot(histories[i].history['loss'], color='blue', label='train')
-        pyplot.plot(histories[i].history['val_loss'], color='orange', label='test')
-        # plot accuracy
-        pyplot.subplot(2, 1, 2)
-        pyplot.title('Classification Accuracy')
-        pyplot.plot(histories[i].history['accuracy'], color='blue', label='train')
-        pyplot.plot(histories[i].history['val_accuracy'], color='orange', label='test')
-    pyplot.show()
-    
-
-
-# summarize model performance
-def summarize_performance(scores):
-    # print summary
-    print('Accuracy: mean=%.3f std=%.3f, n=%d' % (mean(scores)*100, std(scores)*100, len(scores)))
-    # box and whisker plots of results
-    pyplot.boxplot(scores)
-    pyplot.show()
-
-
-# summarize model performance
-def summarize_performance(scores):
-    # print summary
-    print('Accuracy: mean=%.3f std=%.3f, n=%d' % (mean(scores)*100, std(scores)*100, len(scores)))
-    # box and whisker plots of results
-    pyplot.boxplot(scores)
-    pyplot.show()
-    
 
 # run the test harness for evaluating a model
 def run_test_harness():
@@ -348,133 +236,9 @@ def run_test_harness():
     # summarize estimated performance
 
 
-# run the test harness for evaluating a model
-def run_test_harness():
-    # load dataset
-    trainX, trainY, testX, testY = load_dataset()
-    # prepare pixel data
-    trainX, testX = prep_pixels(trainX, testX)
-    # evaluate model
-    scores, histories = evaluate_model(trainX, trainY)
-    # learning curves
-    summarize_diagnostics(histories)
-    # summarize estimated performance
-    summarize_performance(scores)
+#Mode 2: Feed-forward NN 
+############
 
-    # baseline cnn model for mnist
-from numpy import mean
-from numpy import std
-from matplotlib import pyplot
-from sklearn.model_selection import KFold
-from keras.datasets import mnist
-from keras.utils import to_categorical
-from keras.models import Sequential
-from keras.layers import Conv2D
-from keras.layers import MaxPooling2D
-from keras.layers import Dense
-from keras.layers import Flatten
-from keras.optimizers import SGD
- 
-# load train and test dataset
-def load_dataset():
-    # load dataset
-    (trainX, trainY), (testX, testY) = mnist.load_data()
-    # reshape dataset to have a single channel
-    trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
-    testX = testX.reshape((testX.shape[0], 28, 28, 1))
-    # one hot encode target values
-    trainY = to_categorical(trainY)
-    testY = to_categorical(testY)
-    return trainX, trainY, testX, testY
- 
-# scale pixels
-def prep_pixels(train, test):
-    # convert from integers to floats
-    train_norm = train.astype('float32')
-    test_norm = test.astype('float32')
-    # normalize to range 0-1
-    train_norm = train_norm / 255.0
-    test_norm = test_norm / 255.0
-    # return normalized images
-    return train_norm, test_norm
- 
-# define cnn model
-def define_model():
-    model = Sequential()
-    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
-    model.add(MaxPooling2D((2, 2)))
-    model.add(Flatten())
-    model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
-    model.add(Dense(10, activation='softmax'))
-    # compile model
-    opt = SGD(lr=0.01, momentum=0.9)
-    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-    return model
- 
-# evaluate a model using k-fold cross-validation
-def evaluate_model(dataX, dataY, n_folds=5):
-    scores, histories = list(), list()
-    # prepare cross validation
-    kfold = KFold(n_folds, shuffle=True, random_state=1)
-    # enumerate splits
-    for train_ix, test_ix in kfold.split(dataX):
-        # define model
-        model = define_model()
-        # select rows for train and test
-        trainX, trainY, testX, testY = dataX[train_ix], dataY[train_ix], dataX[test_ix], dataY[test_ix]
-        # fit model
-        history = model.fit(trainX, trainY, epochs=10, batch_size=32, validation_data=(testX, testY), verbose=0)
-        # evaluate model
-        _, acc = model.evaluate(testX, testY, verbose=0)
-        print('> %.3f' % (acc * 100.0))
-        # stores scores
-        scores.append(acc)
-        histories.append(history)
-    return scores, histories
- 
-# plot diagnostic learning curves
-def summarize_diagnostics(histories):
-    for i in range(len(histories)):
-        # plot loss
-        pyplot.subplot(2, 1, 1)
-        pyplot.title('Cross Entropy Loss')
-        pyplot.plot(histories[i].history['loss'], color='blue', label='train')
-        pyplot.plot(histories[i].history['val_loss'], color='orange', label='test')
-        # plot accuracy
-        pyplot.subplot(2, 1, 2)
-        pyplot.title('Classification Accuracy')
-        pyplot.plot(histories[i].history['accuracy'], color='blue', label='train')
-        pyplot.plot(histories[i].history['val_accuracy'], color='orange', label='test')
-    pyplot.show()
- 
-# summarize model performance
-def summarize_performance(scores):
-    # print summary
-    print('Accuracy: mean=%.3f std=%.3f, n=%d' % (mean(scores)*100, std(scores)*100, len(scores)))
-    # box and whisker plots of results
-    pyplot.boxplot(scores)
-    pyplot.show()
- 
-# run the test harness for evaluating a model
-def run_test_harness():
-    # load dataset
-    trainX, trainY, testX, testY = load_dataset()
-    # prepare pixel data
-    trainX, testX = prep_pixels(trainX, testX)
-    # evaluate model
-    scores, histories = evaluate_model(trainX, trainY)
-    # learning curves
-    summarize_diagnostics(histories)
-    # summarize estimated performance
-    summarize_performance(scores)
- 
-# entry point, run the test harness
-run_test_harness()
-
-
-########################################
-### Architecture #2 Feed-forward NN ####
-########################################
 import keras
 import numpy as np
 import pandas as pd 
@@ -499,9 +263,9 @@ classifier.compile(optimizer ='adam',loss='binary_crossentropy', metrics =['accu
 classifier.fit(X_train,yt, batch_size=10, epochs=20)
 
 
-#################################################
-### Architecture #3 Recurrent Neural Network ####
-#################################################
+#Mode 3: RNN 
+############
+
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/")
 
@@ -546,8 +310,3 @@ with tf.Session() as sess:
             sess.run(optimizer, feed_dict={X: X_train, y: y_train})
         loss_train, acc_train = sess.run(
             [loss, accuracy], feed_dict={X: X_train, y: y_train})
-        print('Epoch: {}, Train Loss: {:.3f}, Train Acc: {:.3f}'.format(
-            epoch + 1, loss_train, acc_train))
-    loss_test, acc_test = sess.run(
-        [loss, accuracy], feed_dict={X: X_test, y: y_test})
-    print('Test Loss: {:.3f}, Test Acc: {:.3f}'.format(loss_test, acc_test))
